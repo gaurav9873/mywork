2c38de2a74f75df123f68045fd5ca287b17952b4
gaurav9873 <gaurav.mysql@gmail.com>
2019-03-15 00:22:30 +0530

    ddfdf

--- design.inc.php
+++ design.inc.php
@@ -2,13 +2,13 @@
 
 $design_header= <<<__HTML_END
 
-<!-- Add your site's header here -->
+<!-- Add your site's xcxcxcxcxc here -->
 
 __HTML_END;
 
 $design_footer= <<<__HTML_END
 
-<!-- Add your site's footer hexzxzxzxzxzxzre -->
+<!-- Add your cxcxcsite's footer hexzxzxzxzxzxzre -->
 
 __HTML_END;
 
